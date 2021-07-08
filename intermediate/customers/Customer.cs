b64768062b5e8af04de9fc620d5696b31af92611
kolelm18 <57000228+kolelm18@users.noreply.github.com>
2021-07-08 08:36:49 +0200

    Create Customer.cs

---
+++ intermediate/customers/Customer.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Project1
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+        public Customer(int id)
+            :this()
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name)
+            : this()
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+    }
+}
