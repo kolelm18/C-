0b8f58bd1fcbb7ef3068d3e3282ee708a48e5996
kolelm18 <57000228+kolelm18@users.noreply.github.com>
2021-07-12 07:49:05 +0200

    Create Text.cs

---
+++ intermediate/presentation/Text.cs
@@ -0,0 +1,12 @@
+namespace Project1
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set;}
+        public string FontName { get; set; }
+        public void AddHyperLink(string url)
+        {
+            System.Console.WriteLine("We added a link to" + url);
+        }
+    }
+}
