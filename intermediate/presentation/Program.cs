56316036ea2a124225e1e7361f8cbcd1e6e36236
kolelm18 <57000228+kolelm18@users.noreply.github.com>
2021-07-12 07:49:26 +0200

    Create Program.cs

---
+++ intermediate/presentation/Program.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Witdh = 100;
+            text.Copy();
+        } 
+    }
+}
